72f05b47eec12ba4d7d89fc152f5381709f8676f
Niranjana Mohan <Niranjanamohan183@gmail.com>
2025-11-30 10:33:45 +0530

    Day 1 Class Code

---
+++ com/week1/javaBasics/Operators.java
@@ -0,0 +1,21 @@
+package com.week1.javaBasics;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		int c = 30;
+		int result, result2;
+		
+		// variable = exp1 ? exp2 : exp3
+		//if exp1 is true exp2 will be executed
+		//if exp1 is false exp3 will be executed
+		
+		result = (a>b) ? a:b;
+		
+		result2 = ((a>b) ? (a>c) ? a : c: (b>c) ? b:c);
+		
+		System.out.println("Maximum of three "+result2);
+	}
+}
